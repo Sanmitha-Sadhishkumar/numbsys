5ddbcdc5ab7a5f837553a266a588c63185947cf8
Sanmitha-Sadhishkumar <69575577+Sanmitha-Sadhishkumar@users.noreply.github.com>
2020-08-17 21:04:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="numsys", 
+    name="numbsys", 
     version="0.0.1",
     author="Sanmitha Sadhishkumar",
     author_email="sanmithasadhishkumar@gmail.com",
