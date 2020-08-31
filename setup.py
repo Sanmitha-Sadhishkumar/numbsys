71073b6e35661338fe7364a3fdf0b0b5304312a3
Sanmitha-Sadhishkumar <69575577+Sanmitha-Sadhishkumar@users.noreply.github.com>
2020-08-31 23:41:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="numbsys", 
-    version="0.0.1",
+    version="0.0.2",
     author="Sanmitha Sadhishkumar",
     author_email="sanmithasadhishkumar@gmail.com",
     description="This module gives functions that enable conversions between number systems to make python programming easier than before",
